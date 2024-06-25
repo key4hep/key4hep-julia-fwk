5a4fe4cbad233f0dd0018574c6a1e7794f368f47
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-06-25 16:59:39 +0200

    clean up parsing

--- examples/parsing_graphs.jl
+++ examples/parsing_graphs.jl
@@ -1,20 +1,13 @@
-module parsing_graphs
+#!/usr/bin/env julia
 
-using EzXML
-using Graphs
-using MetaGraphs
-using Dagger
-using DaggerWebDash
-include("../../../utilities/GraphMLReader.jl/src/GraphMLReader.jl")
-include("./auxiliary_functions.jl")
-
+using FrameworkDemo
 
 function main()
-    G = parse_graphml(["./data/sequencer_demo/df_sequencer_demo.graphml"])
+    G = FrameworkDemo.parse_graphml(["./data/sequencer_demo/df_sequencer_demo.graphml"])
     G_copy = deepcopy(G)
-    show_graph(G_copy)
+    FrameworkDemo.show_graph(G_copy)
 end
 
-main()
-
-end # module parsing_graphs
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
