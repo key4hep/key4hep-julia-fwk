1765edb8a28147eb9a5d419ec0335f74f5b49622
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-06-27 09:54:13 +0200

    add tests for parsing

---
+++ test/parsing.jl
@@ -0,0 +1,42 @@
+using FrameworkDemo
+using Graphs
+using MetaGraphs
+
+@testset "Parsing" begin
+    graph = FrameworkDemo.parse_graphml(["../data/sequencer_demo/df_sequencer_demo.graphml"])
+    set_indexing_prop!(graph, :original_id)
+
+    # Test the general structure of the graph
+    @test nv(graph) == 14
+    @test ne(graph) == 13
+
+    # Properties of vertex 0
+    id = graph["0", :original_id]
+    vertex = graph.vprops[id]
+    @test vertex[:type] == "Algorithm"
+    @test vertex[:runtime_average_s] ≈ 9.3027e-05
+    @test vertex[:node_id] == "ProducerA"
+
+    # Properties of vertex 7
+    id = graph["7", :original_id]
+    vertex = graph.vprops[id]
+    @test vertex[:type] == "DataObject"
+    @test vertex[:class] == "AnyDataWrapper<int>"
+    @test vertex[:size_average_B] ≈ 8.0
+    @test vertex[:node_id] == "A"
+
+    # Test edges
+    @test has_edge(graph, graph["0", :original_id], graph["7", :original_id])
+    @test has_edge(graph, graph["1", :original_id], graph["8", :original_id])
+    @test has_edge(graph, graph["2", :original_id], graph["9", :original_id])
+    @test has_edge(graph, graph["3", :original_id], graph["10", :original_id])
+    @test has_edge(graph, graph["4", :original_id], graph["11", :original_id])
+    @test has_edge(graph, graph["5", :original_id], graph["12", :original_id])
+    @test has_edge(graph, graph["6", :original_id], graph["13", :original_id])
+    @test has_edge(graph, graph["7", :original_id], graph["1", :original_id])
+    @test has_edge(graph, graph["8", :original_id], graph["2", :original_id])
+    @test has_edge(graph, graph["8", :original_id], graph["6", :original_id])
+    @test has_edge(graph, graph["9", :original_id], graph["3", :original_id])
+    @test has_edge(graph, graph["10", :original_id], graph["4", :original_id])
+    @test has_edge(graph, graph["11", :original_id], graph["5", :original_id])
+end
