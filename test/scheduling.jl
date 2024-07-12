5540470c773cb325c7123bbd06457fe9592c93f0
ottjk <joshott16@gmail.com>
2024-07-12 15:41:49 +0200

    Updated test with calibration

--- test/scheduling.jl
+++ test/scheduling.jl
@@ -40,11 +40,12 @@ end
     ilength(x) = sum(_ -> 1, x) # no standard length for MetaGraphs.filter_vertices iterator
     algorithms_count = ilength(MetaGraphs.filter_vertices(graph, :type, "Algorithm"))
     set_indexing_prop!(graph, :node_id)
+    coefficient = Dagger.@shard FrameworkDemo.calculate_coefficient()
 
     Dagger.enable_logging!(tasknames=true, taskdeps=true)
     _ = Dagger.fetch_logs!() # flush logs
 
-    tasks = FrameworkDemo.schedule_graph(graph)
+    tasks = FrameworkDemo.schedule_graph(graph, coefficient)
     wait.(tasks)
 
     logs = Dagger.fetch_logs!()
