ed478e7d32df9d5f143bcb2aff7299eab6418509
ottjk <joshott16@gmail.com>
2024-07-12 15:36:14 +0200

    Added algorithm calibration and crunching
    Calibration coefficient is stored on each worker and used to crunch for
    average runtime using prime number finder

--- src/scheduling.jl
+++ src/scheduling.jl
@@ -15,8 +15,9 @@ struct MockupAlgorithm
     end
 end
 
-function (alg::MockupAlgorithm)(args...)
+function (alg::MockupAlgorithm)(args...; coefficient::Float64)
     println("Executing $(alg.name)")
+    crunch_for_seconds(alg.runtime, coefficient)
 
     return alg.name
 end
@@ -54,20 +55,20 @@ function is_terminating_alg(graph::AbstractGraph, vertex_id::Int)
     all(is_terminating, successor_dataobjects)
 end
 
-function schedule_algorithm(graph::MetaDiGraph, vertex_id::Int)
+function schedule_algorithm(graph::MetaDiGraph, vertex_id::Int, coefficient::Dagger.Shard)
     incoming_data = get_promises(graph, inneighbors(graph, vertex_id))
     algorithm = MockupAlgorithm(graph, vertex_id)
-    Dagger.@spawn algorithm(incoming_data...)
+    Dagger.@spawn algorithm(incoming_data...; coefficient)
 end
 
-function schedule_graph(graph::MetaDiGraph)
+function schedule_graph(graph::MetaDiGraph, coefficient::Dagger.Shard)
     alg_vertices = MetaGraphs.filter_vertices(graph, :type, "Algorithm")
     sorted_vertices = MetaGraphs.topological_sort(graph)
 
     terminating_results = []
 
     for vertex_id in intersect(sorted_vertices, alg_vertices)
-        res = schedule_algorithm(graph, vertex_id)
+        res = schedule_algorithm(graph, vertex_id, coefficient)
         set_prop!(graph, vertex_id, :res_data, res)
         for v in outneighbors(graph, vertex_id)
             set_prop!(graph, v, :res_data, res)
@@ -80,10 +81,11 @@ function schedule_graph(graph::MetaDiGraph)
 end
 
 function schedule_graph_with_notify(graph::MetaDiGraph,
-    notifications::RemoteChannel,
-    graph_name::String,
-    graph_id::Int)
-    terminating_results = schedule_graph(graph)
+        notifications::RemoteChannel,
+        graph_name::String,
+        graph_id::Int,
+        coefficient::Dagger.Shard)
+    terminating_results = schedule_graph(graph, coefficient)
 
     Dagger.@spawn notify_graph_finalization(notifications, graph_name, graph_id, terminating_results...)
 end
