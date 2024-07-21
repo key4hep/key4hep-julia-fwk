8fad8cbc452a488a4e83e4667a224bdcb1dd701d
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-07-21 10:51:58 +0200

    fix crunch plot, self tune benchmarks

--- benchmark/cpu_crunching.jl
+++ benchmark/cpu_crunching.jl
@@ -1,14 +1,15 @@
 suite["cpu_crunching"] = BenchmarkGroup(["cpu_crunching"])
 
-coef = FrameworkDemo.calculate_coefficients()
 
 for i in exp10.(range(0, stop=6, length=10))
     n = ceil(Int, i)
-    suite["cpu_crunching"]["find_primes"][n] = @benchmarkable FrameworkDemo.find_primes($n)
+    suite["cpu_crunching"]["find_primes"][n] = @benchmarkable FrameworkDemo.find_primes($n) evals = 1 samples = 1
 end
 
+coef = FrameworkDemo.calculate_coefficients()
+
 for i in exp10.(range(-6, stop=1.5, length=10))
-    suite["cpu_crunching"]["crunch_for_seconds"][i] = @benchmarkable FrameworkDemo.crunch_for_seconds($i, $coef)
+    suite["cpu_crunching"]["crunch_for_seconds"][i] = @benchmarkable FrameworkDemo.crunch_for_seconds($i, $coef) evals = 1 samples = 1
 end
 
 function plot_find_primes(results)
@@ -27,8 +28,8 @@ push!(result_processors, plot_find_primes)
 
 function plot_crunch_for_seconds(results)
     crunch_r = sort(collect(results["cpu_crunching"]["crunch_for_seconds"]), by=first)
-    x = first.(primes_r)
-    y = primes_r .|> last .|> minimum .|> time
+    x = first.(crunch_r)
+    y = crunch_r .|> last .|> minimum .|> time |> x -> x * 1e-9
     p = plot(x, y, xaxis=:log10, yaxis=:log10, xlabel="time [s]", ylabel="time [s]",
         title="crunch_for_seconds(t)", label="crunch_for_seconds",
         xguidefonthalign=:right, yguidefontvalign=:top, legend=:topleft)
