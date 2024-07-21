dbdafe04905557e47b2a6b67bdb9e7d4768c7650
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-07-21 10:50:28 +0200

    verbose running and tuning

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -7,9 +7,9 @@ include("cpu_crunching.jl")
 
 if abspath(PROGRAM_FILE) == @__FILE__
     @info "tuning benchmark suite"
-    tune!(suite)
+    tune!(suite, verbose = true)
     @info "running benchmark suite"
-    results = run(suite)
+    results = run(suite, verbose = true)
     println(results)
     for processor in result_processors
         processor(results)
