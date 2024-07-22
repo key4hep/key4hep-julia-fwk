4bc75e45fb8864e0fc1e38ec0236fdec2cfb0ecf
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-07-22 12:13:37 +0200

    fix formatting

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -8,9 +8,9 @@ include("cpu_crunching.jl")
 
 if abspath(PROGRAM_FILE) == @__FILE__
     @info "tuning benchmark SUITE"
-    tune!(SUITE, verbose = true)
+    tune!(SUITE, verbose=true)
     @info "running benchmark SUITE"
-    results = run(SUITE, verbose = true)
+    results = run(SUITE, verbose=true)
     println(results)
     for processor in result_processors
         processor(results)
