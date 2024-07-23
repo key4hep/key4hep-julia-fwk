5d800788bda1c4a793c19977294f1fc328e303d4
ottjk <joshott16@gmail.com>
2024-07-23 11:07:39 +0200

    Added basic runner script

---
+++ scripts/schedule.jl
@@ -0,0 +1,44 @@
+using Distributed
+using Dagger
+using ArgParse
+using FrameworkDemo
+
+function parse_args()
+    s = ArgParseSettings()
+
+    @add_arg_table! s begin
+        "dfg"
+        help = "Input data-flow graph as a GraphML file"
+        arg_type = String
+        required = true
+
+        "event-count"
+        help = "Number of events to be processed"
+        arg_type = Int
+        default = 1
+
+        "max-concurrent"
+        help = "Number of slots for graphs to be scheduled concurrently"
+        arg_type = Int
+        default = 3
+    end
+
+    return ArgParse.parse_args(s)
+end
+
+function main()
+    args = parse_args()
+
+    event_count = args["event-count"]
+    max_concurrent = args["max-concurrent"]
+
+    graph = FrameworkDemo.parse_graphml(args["dfg"])
+    FrameworkDemo.run_events(graph, event_count, max_concurrent)
+end
+
+main()
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    rmprocs!(Dagger.Sch.eager_context(), workers())
+    rmprocs(workers())
+end
