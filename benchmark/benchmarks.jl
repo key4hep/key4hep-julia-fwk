2c0b416154f5e31e7ca6a3bb638503a3f948bce9
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-08-06 09:31:44 +0200

    make results printing info log

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -8,11 +8,11 @@ result_processors = Function[]
 include("suite/cpu_crunching.jl")
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    @info "tuning benchmark SUITE"
+    @info "tuning benchmark suite"
     tune!(SUITE, verbose=true)
-    @info "running benchmark SUITE"
+    @info "running benchmark suite"
     results = run(SUITE, verbose=true)
-    println(results)
+    @info "running benchmark suite" results
     for processor in result_processors
         processor(results)
     end
