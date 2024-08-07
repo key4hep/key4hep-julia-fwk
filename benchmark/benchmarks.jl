d3151ecbe2b9b663408207713a9db644f87a974f
Mateusz Jakub Fila <37295697+m-fila@users.noreply.github.com>
2024-08-07 08:42:47 +0200

    Add benchmarks for cpu crunching (#30)
    * add benchmark for cpu crunching
    
    * verbose running and tuning
    
    * fix crunch plot, self tune benchmarks
    
    * nicer plot styling
    
    * format ticks with sprintf
    
    * stronger typing and more tags in benchmark results
    
    * renamed suite for compatibility with PkgBenchmark
    
    * update .gitignore with pkgbenchmark artifacts
    
    * add benchmark readme
    
    * fix formatting
    
    * fix plot ticks
    
    * create suite subdirectory for benchmarks
    
    * make results printing info log
    
    * Update benchmark/README.md
    
    Co-authored-by: Josh Ott <joshott16@gmail.com>
    
    ---------
    
    Co-authored-by: Josh Ott <joshott16@gmail.com>

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,19 @@
+using FrameworkDemo
+using BenchmarkTools
+using Plots
+
+SUITE = BenchmarkGroup()
+result_processors = Function[]
+
+include("suite/cpu_crunching.jl")
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    @info "tuning benchmark suite"
+    tune!(SUITE, verbose=true)
+    @info "running benchmark suite"
+    results = run(SUITE, verbose=true)
+    @info "running benchmark suite" results
+    for processor in result_processors
+        processor(results)
+    end
+end
---
+++ benchmark/suite/cpu_crunching.jl
@@ -0,0 +1,54 @@
+import Printf
+
+SUITE["cpu_crunching"] = BenchmarkGroup(["cpu_crunching"])
+
+SUITE["cpu_crunching"]["find_primes"] = BenchmarkGroup(["find_primes"])
+for i in exp10.(range(0, stop=6, length=10))
+    n = ceil(Int, i)
+    SUITE["cpu_crunching"]["find_primes"][n] = @benchmarkable FrameworkDemo.find_primes($n) evals = 1 samples = 1
+end
+
+SUITE["cpu_crunching"]["crunch_for_seconds"] = BenchmarkGroup(["crunch_for_seconds"])
+coef = FrameworkDemo.calculate_coefficients()
+for i in exp10.(range(-6, stop=1.5, length=10))
+    SUITE["cpu_crunching"]["crunch_for_seconds"][i] = @benchmarkable FrameworkDemo.crunch_for_seconds($i, $coef) evals = 1 samples = 1
+end
+
+function log_ticks(range)
+    start = floor(log10(minimum(range)))
+    stop = ceil(log10(maximum(range)))
+    return 10.0 .^ (start:stop)
+end
+
+function plot_find_primes(results::BenchmarkGroup)
+    primes_r = sort(collect(results["cpu_crunching"]["find_primes"]), by=first)
+    x = first.(primes_r)
+    y = primes_r .|> last .|> minimum .|> time |> x -> x * 1e-9
+    p = plot(x, y, xaxis=:log10, yaxis=:log10, xlabel="n", ylabel="time [s]",
+        title="find_primes(n)", label="find_primes",
+        marker=(:circle, 5), linewidth=3,
+        xticks=log_ticks(x), yticks=log_ticks(y),
+        xguidefonthalign=:right, yguidefontvalign=:top, legend=:topleft)
+    filename = "bench_find_primes.png"
+    savefig(p, filename)
+    @info "Results of benchmark cpu_crunching/find_primes written to $filename"
+end
+
+push!(result_processors, plot_find_primes)
+
+function plot_crunch_for_seconds(results::BenchmarkGroup)
+    crunch_r = sort(collect(results["cpu_crunching"]["crunch_for_seconds"]), by=first)
+    x = first.(crunch_r)
+    y = crunch_r .|> last .|> minimum .|> time |> x -> x * 1e-9
+    p = plot(x, (y - x) ./ x, xaxis=:log10, xlabel="t [s]", ylabel="Time relative error",
+        yformatter=x -> Printf.@sprintf("%.1f%%", 100 * x),
+        xticks=log_ticks(x),
+        title="crunch_for_seconds(t)", label="crunch_for_seconds",
+        marker=(:circle, 5), linewidth=3,
+        xguidefonthalign=:right, yguidefontvalign=:top, legend=:bottomright)
+    filename = "bench_crunch_for_seconds.png"
+    savefig(p, filename)
+    @info "Results of benchmark cpu_crunching/crunch_for_seconds written to $filename"
+end
+
+push!(result_processors, plot_crunch_for_seconds)
