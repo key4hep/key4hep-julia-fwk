693cd746fe53efde32846f36483098f0d4a36204
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-08-08 19:32:48 +0200

    skip false positive stale deps

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -3,5 +3,10 @@ using FrameworkDemo
 
 @testset "Aqua.jl" begin
     Aqua.test_all(FrameworkDemo;
-                  ambiguities = false,)
+                  ambiguities = false,
+                  stale_deps = (;
+                                ignore = [:ArgParse, # bin/
+                                    :BenchmarkTools, # benchmarks.jl
+                                    :Plots, # benchmarks.jl, Dagger ext
+                                    :DataFrames]))
 end
