560f9dc80e9fe306f14fa46526ae361eef036ca1
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-08-27 20:00:11 +0200

    removed unused functions and deps

--- src/parsing.jl
+++ src/parsing.jl
@@ -3,30 +3,6 @@ using MetaGraphs
 include("../deps/GraphMLReader.jl/src/GraphMLReader.jl")
 
 function parse_graphml(filename::String)::MetaDiGraph
-    GraphMLReader.loadgraphml(filename, "G")
+    return GraphMLReader.loadgraphml(filename, "G")
 end
 
-function show_graph(G)
-    for v in Graphs.vertices(G)
-        println("Node: ")
-        print("Node type: ")
-        println(get_prop(G, v, :type))
-        if has_prop(G, v, :class)
-            print("Node class: ")
-            println(get_prop(G, v, :class))
-        end
-        if has_prop(G, v, :runtime_average_s)
-            print("Average runtime [s]: ")
-            println(get_prop(G, v, :runtime_average_s))
-        end
-        if has_prop(G, v, :size_average_B)
-            print("Average size [B]: ")
-            println(get_prop(G, v, :size_average_B))
-        end
-        print("Original name: ")
-        println(get_prop(G, v, :original_id))
-        print("Node name: ")
-        println(get_prop(G, v, :node_id))
-        println()
-    end
-end
