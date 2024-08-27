9b3e4e368be4dfe8b96d53e24b06040bdfa8b441
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-08-27 20:32:33 +0200

    automatically convert dot to other formats

--- src/visualization.jl
+++ src/visualization.jl
@@ -1,8 +1,20 @@
 import Colors
+import GraphViz
+import FileIO
 
 function save_logs_dot(logs, path::String)
-    open(path, "w") do io
-        ModGraphVizSimple.show_logs(io, logs, :graphviz_simple)
-        @info "Written logs dot graph to $path"
+    if splitext(path)[2] == ".dot"
+        open(path, "w") do io
+            ModGraphVizSimple.show_logs(io, logs, :graphviz_simple)
+            @info "Written logs dot graph to $path"
+        end
+    else
+        buffer = IOBuffer()
+        ModGraphVizSimple.show_logs(buffer, logs, :graphviz_simple)
+        dot = String(take!(buffer))
+        graphviz = GraphViz.Graph(dot)
+        GraphViz.layout!(graphviz; engine = "dot")
+        FileIO.save(path, graphviz)
+        @info "Written logs graph to $path"
     end
 end
