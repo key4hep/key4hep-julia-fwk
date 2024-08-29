c49183f3c4622146c0b445d70e79a7d5db6e8493
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-08-29 09:25:20 +0200

    add configuration for Dagger MultiEventLog

--- src/logging.jl
+++ src/logging.jl
@@ -6,12 +6,23 @@ function configure_LocalEventLog()
     ctx.log_sink = log
 end
 
-function fetch_logs!()
-    return Dagger.fetch_logs!()
+function enable_logging!()
+    Dagger.enable_logging!(tasknames = true,
+                           taskfuncnames = true,
+                           taskdeps = true,
+                           taskargs = true,
+                           taskargmoves = true,
+                           taskresult = true,
+                           taskuidtotid = true,
+                           tasktochunk = true)
 end
 
 function disable_logging!()
     Dagger.disable_logging!()
+end
+
+function fetch_logs!()
+    return Dagger.fetch_logs!()
 end
 
 function dispatch_begin_msg(index)
