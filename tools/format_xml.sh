ded0529e81828e5d1209ad85d5f22afe2c48e122
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-10-11 23:08:52 +0200

    remove ignoring deps

--- tools/format_xml.sh
+++ tools/format_xml.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-find . -name "*.graphml" -type f -not -path "./deps/*" -exec xmllint --output '{}' --format '{}' \;
+find . -name "*.graphml" -type f -exec xmllint --output '{}' --format '{}' \;
