dc9012daa5fccbd74eb627be20e5ec15cd5301a5
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-10-11 14:04:33 +0200

    use `[sources]` to specify dependency on  GraphMLReader

--- src/parsing.jl
+++ src/parsing.jl
@@ -1,6 +1,6 @@
 using Graphs
 using MetaGraphs
-include("../deps/GraphMLReader.jl/src/GraphMLReader.jl")
+import GraphMLReader
 
 function parse_graphml(filename::String)::MetaDiGraph
     return GraphMLReader.loadgraphml(filename, "G")
--- test/Aqua.jl
+++ test/Aqua.jl
@@ -5,7 +5,6 @@ using Aqua
 @testset "Aqua.jl" begin
     Aqua.test_all(FrameworkDemo;
                   ambiguities = false,
-                  stale_deps = (;
-                                ignore = [:ArgParse, # bin/
-                                    :BenchmarkTools]))
+                  stale_deps = (; ignore = [:ArgParse]),# bin/
+                  persistent_tasks = (; broken = true)) # GraphMLReader [8e6830a9] has no known versions!
 end
