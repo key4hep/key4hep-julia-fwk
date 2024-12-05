b77d6d638227a293e80d75225e4c3fd8bb893bdb
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-12-05 11:03:56 +0100

    use DistributedNext

--- src/FrameworkDemo.jl
+++ src/FrameworkDemo.jl
@@ -1,5 +1,10 @@
 module FrameworkDemo
-
+import Preferences
+if Preferences.load_preference("Dagger", "distributed-package") == "DistributedNext"
+     using DistributedNext
+else
+     using Distributed
+end
 include("logging.jl")
 include("parsing.jl")
 include("scheduling.jl")
