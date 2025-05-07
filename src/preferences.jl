45e458b4ffbe19a9affa2d65b86c3b4155499753
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-05-07 20:52:05 +0200

    reformat

--- src/preferences.jl
+++ src/preferences.jl
@@ -6,6 +6,6 @@ function set_distributed_package!(package_name::String)
         throw(ArgumentError("Invalid distributed package: $(package_name)"))
     end
     Dagger.set_distributed_package!(package_name)
-    Preferences.@set_preferences!("distributed-package" => package_name)
+    Preferences.@set_preferences!("distributed-package"=>package_name)
     @info("Preferences updated, restart your Julia session for this change to take effect")
 end
