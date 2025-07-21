274c4fd81797e90d00c0a1b9651a092976658576
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-07-22 00:14:35 +0200

    bring back scheduling tests

--- test/scheduling.jl
+++ test/scheduling.jl
@@ -13,6 +13,45 @@ using Logging
     is_fast = "no-fast" ∉ ARGS
     coefficients = FrameworkDemo.calibrate_crunch(; fast = is_fast)
 
+    df = FrameworkDemo.mockup_dataflow(graph)
+    event = FrameworkDemo.Event(df)
+
+    test_logger = TestLogger()
+    with_logger(test_logger) do
+        FrameworkDemo.schedule_graph!(event, coefficients)
+    end
+    @testset "Timeline" begin
+        # store order of appearance in the log by message
+        log_position = Dict{String, Int}()
+        for (i, record) in enumerate(test_logger.logs)
+            # match name from message "Executing $name $event_number" logged by mockup algs
+            m = match(r"Executing (\w+)", record.message)
+            if m !== nothing
+                component = m.captures[1]
+                log_position[component] = i
+            end
+        end
+
+        @test log_position["ProducerA"] < log_position["TransformerAB"]
+        @test log_position["ProducerBC"] < log_position["TransformerAB"]
+        @test log_position["ProducerBC"] < log_position["ConsumerBC"]
+        @test log_position["ProducerBC"] < log_position["TransformerC"]
+        @test log_position["ProducerBC"] < log_position["ConsumerCD"]
+        @test log_position["TransformerAB"] < log_position["ConsumerE"]
+        @test log_position["TransformerAB"] < log_position["ConsumerCD"]
+    end
+
+    @testset "Dependencies" begin
+        get_id(name) = graph[name, :node_id]
+        get_parent(name) = FrameworkDemo.get_result(event, graph[name, :node_id])
+        @test get_parent("a") == get_id("ProducerA")
+        @test get_parent("b") == get_id("ProducerBC")
+        @test get_parent("c") == get_id("ProducerBC")
+        @test get_parent("d") == get_id("TransformerAB")
+        @test get_parent("e") == get_id("TransformerAB")
+        @test get_parent("f") == get_id("TransformerAB")
+        @test get_parent("g") == get_id("TransformerC")
+    end
     @testset "Pipeline" begin
         event_count = 5
         data_flow = FrameworkDemo.mockup_dataflow(graph)
